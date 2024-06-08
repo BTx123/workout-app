eef4b82eb818a9f71b709cc83831823ddeafc9f3
Brian Tom <brian.tom@quartus.com>
2024-06-07 20:23:21 -0700

    updates

--- WorkoutApp/WorkoutApp.DAL/Entities/Set.cs
+++ WorkoutApp/WorkoutApp.DAL/Entities/Set.cs
@@ -6,24 +6,24 @@ namespace WorkoutApp.DAL.Entities;
 public class Set : EntityBase
 {
     [Column("set_group_id")]
-    public int SetGroupId { get; init; }
+    public int SetGroupId { get; set; }
 
-    public SetGroup SetGroup { get; init; }
+    public SetGroup SetGroup { get; set; }
 
     [Column("started_at")]
-    public DateTime StartedAt { get; init; }
+    public DateTime StartedAt { get; set; }
 
     [Column("stopped_at")]
-    public DateTime StoppedAt { get; init; }
+    public DateTime StoppedAt { get; set; }
 
     [Column("repetitions")]
     [Range(0, double.PositiveInfinity)]
-    public int Repetitions { get; init; }
+    public int Repetitions { get; set; }
 
     [Column("mass_kg")]
     [Range(0, double.PositiveInfinity)]
-    public decimal MassKg { get; init; }
+    public decimal MassKg { get; set; }
 
     [Column("is_amrap")]
-    public bool IsAmrap { get; init; }
+    public bool IsAmrap { get; set; }
 }
